26d1ddb841d92fa918abba1b8581195b57569b12
Denis Elistratov <deniska2003elistratov@mail.ru>
2025-01-06 01:08:39 +0300

    delete useless files

--- src/main/java/ru/char1lot/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.char1lot;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
-    }
-}
--- src/main/java/ru/char1lot/frontend/Front.java
+++ src/main/java/ru/char1lot/frontend/Front.java
@@ -3,9 +3,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Front {
 
